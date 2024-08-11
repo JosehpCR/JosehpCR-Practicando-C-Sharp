5b5769b6d74259cb410ea32f06e6d503f60dddd1
Camilo_R <josephcamilo2002@gmail.com>
2024-08-11 12:02:50 -0500

    Hasta video 16

---
+++ Condicionales/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Sueldobasico = 250000;
+            double Sueldo2;
+            Console.WriteLine("Digite su sueldo");
+            Double.TryParse(Console.ReadLine(),out Sueldo2);
+            if (Sueldobasico>=Sueldo2)
+            {
+                Console.WriteLine("Sueldo Alto");
+            }
+            else
+            {
+                Console.WriteLine("Negativo");
+            }
+            Console.WriteLine("Tienes carnet?");
+            string respuesta = Console.ReadLine();
+            int comparar = String.Compare(respuesta,"si",true);
+            if (comparar == 0)
+            {
+                Console.WriteLine("Afirmativo");
+            }
+            else
+            {
+                Console.WriteLine("Negativo");
+            }
+           
+            int num1 =13; int num2 = 15;
+            var Respuesta = new int();
+            var resultado = Sumar(num1,num2,ref Respuesta);
+
+        }
+       static double Sumar(int n1, int n2, ref int resultado)
+        {
+
+            resultado = n1 + n2;
+            n1 = 200;
+            return 0.0;
+        }
+    }
+}
---
+++ Proyecto_Metodos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = 7; double valor2 = 8.3; double valor3 = 9.5;
+            // Console.WriteLine(Suma(12.0,11.5));
+            Console.WriteLine(SumaOpcional(valor1,valor2));
+        }
+        static double Suma(double v1, double v2)
+        {
+            return v1 + v2;
+        }
+
+        private static double SumaOpcional(int num1, double num2, double num3=0)
+        {
+            return num1 + num2 + num3;
+        }
+
+        static double Suma(int num1, double num2)
+        {
+            return num1 + num2;
+         }
+    }
+}
