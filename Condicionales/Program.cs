7d5de3bd0abf23a98105c48d0bcad8caa4ba45d6
Camilo_R <josephcamilo2002@gmail.com>
2024-08-13 22:31:37 -0500

    Hasta el video 22 exepciones

--- Condicionales/Program.cs
+++ Condicionales/Program.cs
@@ -7,33 +7,36 @@ namespace Condicionales
     {
         static void Main(string[] args)
         {
-            double Sueldobasico = 250000;
-            double Sueldo2;
-            Console.WriteLine("Digite su sueldo");
-            Double.TryParse(Console.ReadLine(),out Sueldo2);
-            if (Sueldobasico>=Sueldo2)
-            {
-                Console.WriteLine("Sueldo Alto");
-            }
-            else
-            {
-                Console.WriteLine("Negativo");
-            }
-            Console.WriteLine("Tienes carnet?");
-            string respuesta = Console.ReadLine();
-            int comparar = String.Compare(respuesta,"si",true);
-            if (comparar == 0)
-            {
-                Console.WriteLine("Afirmativo");
-            }
-            else
-            {
-                Console.WriteLine("Negativo");
-            }
+            //double Sueldobasico = 250000;
+            //double Sueldo2;
+            //Console.WriteLine("Digite su sueldo");
+            //Double.TryParse(Console.ReadLine(),out Sueldo2);
+            //if (Sueldobasico>=Sueldo2)
+            //{
+            //    Console.WriteLine("Sueldo Alto");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Negativo");
+            //}
+            //Console.WriteLine("Tienes carnet?");
+            //string respuesta = Console.ReadLine();
+            //int comparar = String.Compare(respuesta,"si",true);
+            //if (comparar == 0)
+            //{
+            //    Console.WriteLine("Afirmativo");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Negativo");
+            //}
            
             int num1 =13; int num2 = 15;
             var Respuesta = new int();
             var resultado = Sumar(num1,num2,ref Respuesta);
+
+            Edades();
+            
 
         }
        static double Sumar(int n1, int n2, ref int resultado)
@@ -43,5 +46,23 @@ namespace Condicionales
             n1 = 200;
             return 0.0;
         }
+        static void Edades()
+        {
+            Console.WriteLine("Escribe tu edad");
+            int Edad = Int32.Parse (Console.ReadLine());
+
+            if (Edad < 18) Console.WriteLine("Usted es un niño");
+
+            else if (Edad < 30) Console.WriteLine("Usted es un joven");
+            else if (Edad < 60) Console.WriteLine("Usted es un señor");
+            else
+            {
+                Console.WriteLine("Usted es viejo");
+            }
+        }
+
+
+
+
     }
 }
---
+++ Proyecto_Do-While/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random numero = new Random();
+        int numeroAleatorio = numero.Next(0,100);
+        Console.WriteLine("Digite un número para intentar adivinar el número escondido");
+        int numeroDigitado;
+        int cont=0;
+        
+        do
+        {
+            cont++;
+            numeroDigitado = Int32.Parse(Console.ReadLine());
+            if (numeroDigitado > numeroAleatorio) Console.WriteLine("Digite un número menor al anterior digitado");
+            if (numeroDigitado < numeroAleatorio) Console.WriteLine("Digite un número mayor al anterior digitado");
+        } while (numeroDigitado != numeroAleatorio);
+        Console.WriteLine("Felicidades ha encontrado el número: " + numeroAleatorio);
+        Console.WriteLine("Lo ha intentado: "+ cont);
+        Console.ReadKey();
+            
+            
+    }
+}
