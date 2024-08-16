2a3d59bac4fe02d124e2befefc2b27024203e802
Camilo_R <josephcamilo2002@gmail.com>
2024-08-16 09:40:24 -0500

    Hasta checked Video 24

--- Proyecto_Do-While/Program.cs
+++ Proyecto_Do-While/Program.cs
@@ -11,14 +11,29 @@
         do
         {
             cont++;
-            numeroDigitado = Int32.Parse(Console.ReadLine());
+            try
+            {
+                numeroDigitado = Int32.Parse(Console.ReadLine());
+            }
+            // catch (FormatException exception) *Tipos diferentes de exepciones
+            // catch (Exception exception) Tipo general de e
+            catch (Exception exception) when (exception.GetType() != typeof(FormatException))
+            {
+                Console.WriteLine("Ha ocurrido un error, se tomo como numero introducido el 0");
+                numeroDigitado = 0;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Has introducido letras");
+                numeroDigitado = 0;
+            }
+           
             if (numeroDigitado > numeroAleatorio) Console.WriteLine("Digite un número menor al anterior digitado");
             if (numeroDigitado < numeroAleatorio) Console.WriteLine("Digite un número mayor al anterior digitado");
         } while (numeroDigitado != numeroAleatorio);
         Console.WriteLine("Felicidades ha encontrado el número: " + numeroAleatorio);
         Console.WriteLine("Lo ha intentado: "+ cont);
         Console.ReadKey();
-            
-            
+        
     }
 }
---
+++ Proyecto_UsoChecked/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Proyecto_UsoChecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            checked
+            {
+                int numero = int.MaxValue;
+                int resultado = numero + 20;
+                Console.WriteLine(resultado);
+            }
+        }
+    }
+}
+
