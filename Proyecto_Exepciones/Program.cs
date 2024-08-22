3204ddd8a7587f4bb08e817fb8082d0fff766dca
Camilo_R <josephcamilo2002@gmail.com>
2024-08-21 23:11:36 -0500

    INICIOS DE POO

---
+++ Proyecto_Exepciones/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Proyecto_Exepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el numero de un mes del año");
+            int NumeroMes = int.Parse(Console.ReadLine());
+            try
+            {
+               Console.WriteLine(NombreDelMes(NumeroMes)); 
+               Console.WriteLine("Se continua ejecutando el programa");
+            }
+            catch (Exception e)
+            {
+               Console.WriteLine("El mensaje de error es: " + e.Message);
+            }
+        }
+        public static string NombreDelMes(int mes)
+        {
+            switch (mes)
+            {
+                case 1:
+                    return "Enero";
+                case 2:
+                    return "Febrero";
+                case 3:
+                    return "Marzo";
+                case 4:
+                    return "Abril";
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+    
+        }
+    
+    }
+}
+
+
---
+++ Proyecto_Setters_Getters/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Proyecto_Setters_Getters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Coche coche1 = new Coche(4, false, true, 453, 534);
+        Console.WriteLine(coche1.GetInfoCoche());
+
+    }
+
+    class Coche
+    {
+        private int ruedas;
+        private bool climatizador;
+        private bool tapiceria;
+        private double ancho;
+        private double largo;
+        
+        public Coche(int ruedas, bool climatizador, bool tapiceria, double ancho, double largo)
+        {
+            this.ruedas = ruedas;
+            this.climatizador = climatizador;
+            this.tapiceria = tapiceria;
+            this.ancho = ancho;
+            this.largo = largo;
+        }
+        public string GetInfoCoche()
+        {
+            return "Ruedas: " + ruedas + "\nClimatizador: " + climatizador + "\nTapiceria: " + tapiceria +
+                   "\nAncho: " + ancho + "\nLargo: " + largo;
+        }
+        
+    }
+}
+
